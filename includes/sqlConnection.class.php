6beb36d3872a0f1a1661a8bcb06030bd991b9b91
me <me@sum.co.th>
2019-06-22 14:18:08 +0700

    Refactor connection class

--- includes/sqlConnection.class.php
+++ includes/sqlConnection.class.php
@@ -1,18 +1,29 @@
 <?php
 	class Connect {
 		public $connect;
+		private $query;
+		private $fetchVar;
 		function __construct () {
 			$this->connect = mysqli_connect(DB_HOST,DB_USER,DB_PWD, DB_NAME);
 			mysqli_query($this->connect, "SET NAMES utf8");
 		}
 
-		function getConnection () {
+		public function getConnection () {
 			return $this->connect;
 		}
 
-		function query ($query) {
-			return mysqli_query($this->connect, $query);
+		public function query ($query) {
+			$this->query = mysqli_query($this->connect, $query);
+			return $this->query;
 		}
 
+		public function next_record () {
+			$this->fetchVar = mysqli_fetch_assoc($this->query);
+			return is_array($this->fetchVar);
+		}
+
+		public function getValue ($key) {
+			return $this->fetchVar[$key];
+		}
 	}
 ?>
