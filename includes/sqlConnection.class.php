c51fa69fde607c67765a3f0013f274a9097fa3ff
me <me@sum.co.th>
2019-06-22 12:15:59 +0700

    Add Connection function

--- includes/sqlConnection.class.php
+++ includes/sqlConnection.class.php
@@ -3,12 +3,16 @@
 		public $connect;
 		function __construct () {
 			$this->connect = mysqli_connect(DB_HOST,DB_USER,DB_PWD, DB_NAME);
-			mysqli_query($this->connect, "SET NAMES ".DB_CHAR)
 			mysqli_query($this->connect, "SET NAMES utf8");
 		}
 
 		function getConnection () {
 			return $this->connect;
 		}
+
+		function query ($query) {
+			return mysqli_query($this->connect, $query);
+		}
+
 	}
 ?>
