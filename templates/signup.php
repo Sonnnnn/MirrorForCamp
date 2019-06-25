d016924f7a954cb1f5028d048d860993f513d4fc
Son <sitthavee.t@gmail.com>
2019-06-25 09:59:23 +0700

    Update signup again

--- templates/signup.php
+++ templates/signup.php
@@ -1,42 +1,3 @@
-
-<html>
-<head>
-	<title>Mirror for Camp - เว็บเขียนกระจกเงาออนไลน์</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-
-</head>
-<body>
-
-    <?php if (isset($_POST['form_submitted'])): ?> 
-
-        <h2>Thank You <?php echo $_POST['camp_name']; ?> </h2>
-
-        <p>You have been registered as
-            <?php echo $_POST['camp_name'] . ' ' . $_POST['camp_name']; ?>
-        </p>
-
-        <p>Go <a href="index.html">back</a> to the main page</p>
-
-        <?php else: ?>
-
-        <h2>เปิดใช้งานกระจกเงาออนไลน์ สำหรับค่ายของคุณ</h2>
-        
-        <form action="signup.php" method="POST">
-                <div class="form-row">
-                        <div class="col-12 col-md-9 mb-2 mb-md-0">
-        ชื่อค่าย <input type="text" class="form-control form-control-lg font" name="camp_name"><br>
-        <input type="hidden" name="form_submitted" value="1" />
-        <input type="submit" value="Submit">
-        </form>
-
-      <?php endif; ? > 
-
-</body> 
-</html>
-
-
-/*
-
 <html>
 <html lang="en">
 
@@ -56,7 +17,3 @@
 
 </body>
 </html>
-
-*/
-
-
