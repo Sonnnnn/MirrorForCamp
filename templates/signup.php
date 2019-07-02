a7651896ff9dc094e46e7ff1fdaa3514e50ac7dc
Son <sitthavee.t@gmail.com>
2019-07-02 11:48:51 +0700

    Add random camp_code

--- templates/signup.php
+++ templates/signup.php
@@ -1,3 +1,13 @@
+<?php
+
+$camp_code = mt_rand(1000,9999);
+
+/* if $camp_code ไม่ซ้ำกับ database MYSQL ก็ไปต่อ ถ้าซ้ำก็วนลูป */
+
+/* save $camp_code ลง database MYSQL ใน row เดียวกับ camp_name ที่ได้มา */
+
+?>
+
 <html>
 <html lang="en">
 
@@ -10,10 +20,22 @@
 
   <title>Mirror for Camp - เว็บเขียนกระจกเงาออนไลน์</title>
 </head>
+
+<style>
+.font {
+  font-family: 'Kanit', sans-serif;
+}
+
+</style>
+
+
 <body>
 
-<h1>ยินดีด้วย <?php echo $_GET["camp_name"]; ?></h1><br>
-<p>รหัสค่ายของคุณคือ xxxx</p>
+<div class="font">
+  <h1>ยินดีด้วย "<?php echo $_GET["camp_name"]; ?>" ได้เปิดใช้งานแล้ว</h1><br>
+    <p>รหัสค่ายของคุณคือ <strong><?php echo $camp_code; ?><strong></p>
+</div>
+
 
 </body>
 </html>
