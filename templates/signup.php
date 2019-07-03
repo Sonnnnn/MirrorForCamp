3bde18eafea1ab832b6231e96edda7ee381295a8
LouisPN <poonchawit.ngokprasertkul@gmail.com>
2019-07-03 11:53:27 +0700

    Initial camp code validation script

--- templates/signup.php
+++ templates/signup.php
@@ -4,8 +4,16 @@ $camp_code = mt_rand(1000,9999);
 
 /* if $camp_code ไม่ซ้ำกับ database MYSQL ก็ไปต่อ ถ้าซ้ำก็วนลูป */
 
-/* save $camp_code ลง database MYSQL ใน row เดียวกับ camp_name ที่ได้มา */
+$isCodeExist = true;
+while ($isCodeExist) {
+    $query = ""; /* Will Write SQL command to get camp that has same code*/
+    if (true) /* Will write condition to check SQL get anything or not, for now ALWAYS set as true to prevent infinite loop */
+      $isCodeExist = false;
+}
 
+/* save $camp_code ลง database MYSQL ใน row เดียวกับ camp_name ที่ได้มา */
+$query = ""; /* Will write SQL command to write camp name and code to database */
+$result = true; /* Will write validator if record insert successfully */
 ?>
 
 <html>
@@ -32,8 +40,19 @@ $camp_code = mt_rand(1000,9999);
 <body>
 
 <div class="font">
+  <?php
+    if ($result) {
+  ?>
   <h1>ยินดีด้วย "<?php echo $_GET["camp_name"]; ?>" ได้เปิดใช้งานแล้ว</h1><br>
     <p>รหัสค่ายของคุณคือ <strong><?php echo $camp_code; ?><strong></p>
+  <?php
+    }
+    else {
+  ?>
+  <h1>ระบบไม่สามารถสร้างค่ายได้ กรุณาลองใหม่อีกครั้ง</h1>
+  <?php
+    }
+  ?>
 </div>
 
 
