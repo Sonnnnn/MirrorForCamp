07943b7c50bd70d78ac060f51fbb42248582eac6
Son <sitthavee.t@gmail.com>
2019-09-21 22:39:40 -0400

    leave Message form

--- templates/dashboard.php
+++ templates/dashboard.php
@@ -47,10 +47,7 @@
               while ($connect->next_record()) {
                 ?>
                   <div class="envelope-container col-lg-3 col-sm-4 col-6 text-center margin">
-                
-
-                  <!---ขอปิด hyperlink href ไปก่อน เพื่อทดสอบ pop-up modal -->
-                  <!--  <a href="leaveMessage.php?attendee_id=<?=$connect->getValue("id")?>"> -->
+                    <a href="leaveMessage.php?attendee_id=<?=$connect->getValue("id")?>">
 
                       <div class="envelope-icon">
                         <div class="features-icons-icon d-flex">
@@ -60,43 +57,7 @@
                       <div id="attendee-name"> <h5> <?=$connect->getValue("display_name")?> </h5> </div>
                       <div id="attendee-caption"><?=$connect->getValue("caption")?></div>
 
-
-  <!--ความพยายามทำ pop up modal -->
- 
-                      <button type="button" class="btn btn-success" data-toggle="modal" data-target="#leaveMessage" data-whatever="@test">เขียนข้อความ</button>
-                  
-                  <div class="modal fade" id="leaveMessage" tabindex="-1" role="dialog" aria-labelledby="leaveMessage" aria-hidden="true">
-                   <div class="modal-dialog" role="document">
-                  <div class="modal-content">
-                   <div class="modal-header">
-                     <h5 class="modal-title" id="leaveMessage">เขียนข้อความ</h5>
-                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                       <span aria-hidden="true">&times;</span>
-                     </button>
-                   </div>
-                   <div class="modal-body">
-                     <form>
-                       <div class="form-group">
-                         <label for="recipient-name" class="col-form-label">เจ้าของกระจกเงา</label>
-                         <input type="text" class="form-control" id="recipient-name">
-                       </div>
-                       <div class="form-group">
-                         <label for="message-text" class="col-form-label">ข้อความ</label>
-                         <textarea class="form-control" id="message-text"></textarea>
-                       </div>
-                     </form>
-                   </div>
-                   <div class="modal-footer">
-                     <button type="button" class="btn btn-secondary" data-dismiss="modal">ยกเลิก</button>
-                     <button type="button" class="btn btn-primary">ส่งข้อความ</button>
-                   </div>
-                 </div>
-               </div>
-             </div>
-
-            
-                  <!---ขอปิด hyperlink href ไปก่อน เพื่อทดสอบ pop-up modal -->
-                  <!--  </a> -->
+                    </a>
                   </div>
                 <?php
               }
@@ -121,24 +82,3 @@
     ?>
   </body>
 </html>
-
-
-
-<script>
-
-
-
-$('#leaveMessage').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
-})
-
-$('#leaveMessage').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget) // Button that triggered the modal
-  var recipient = button.data('whatever') // Extract info from data-* attributes
-  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-  var modal = $(this)
-  modal.find('.modal-title').text('New message to ' + recipient)
-  modal.find('.modal-body input').val(recipient)
-})
-</script>
--- templates/leaveMessage.php
+++ templates/leaveMessage.php
@@ -4,7 +4,7 @@
   include(__DIR__."/../includes/mirror.engine.class.php");
 
   $attendee_id = $_GET["attendee_id"];
-  }
+
 ?>
 <html>
   <head>
@@ -30,29 +30,28 @@
 
 
       <div class="card-body">
-        <form method="POST"> 
-          <input type="hidden" name="camp_id" value="<?=$camp_id?>">
-
-                <div class="form-group col-12 col-md-9 mb-2 mb-md-0">
-                <label>ชื่อเล่น *</label>
-                <input type="text" name="display_name" class="form-control form-control-lg font" required>
-                </div>
+        <form action="/dashboard.php" method="POST"> 
+              <div class="form-group col-12 col-md-9 mb-2 mb-md-0">
+                  <label for="display_name">ชื่อเล่น *</label>
+                  <input type="text" name="display_name" id="display_name" class="form-control form-control-lg font" required>
+          
                   <br>
-                <div class="form-group col-12 col-md-9 mb-2 mb-md-0">
-                <label>คำอธิบาย</label>
-                <input type="text" name="caption" class="form-control form-control-lg font">
-                <small class="form-text text-muted">คำอธิบายเพิ่มเติมในกรณีที่ชื่อของคุณซ้ำกับคนอื่น เช่น โบสถ์สะพานเหลือง หรือ ม.เชียงใหม่</small>
-                </div>
-                <br>
-                <div class="form-group col-12 col-md-9 mb-2 mb-md-0">
-                <label>รหัสเปิดสมุด *</label>
-                <input type="password" name="password" class="form-control form-control-lg font" required>
-                <small class="form-text text-muted">รหัสนี้มีไว้สำหรับเปิดสมุดกระจกส่วนตัวของคุณ เพื่อป้องกันไม่ให้เพื่อนคนอื่นเข้ามาดูข้อความส่วนตัวในสมุดกระจกของคุณ</small>
-                </div>
-              <button type="submit" name="submit" value="submit" class="btn button font">เพิ่มสมุดกระจกเงา</button>
+              
+                  <label for="caption">คำอธิบาย</label>
+                  <input type="text" name="caption" id="caption" class="form-control form-control-lg font">
+                  <small class="form-text text-muted">คำอธิบายเพิ่มเติมในกรณีที่ชื่อของคุณซ้ำกับคนอื่น เช่น โบสถ์สะพานเหลือง หรือ ม.เชียงใหม่</small>
+                
+                  <br>
 
+                 <label for="password">รหัสเปิดสมุด *</label>
+                 <input type="password" name="password" id="password" class="form-control form-control-lg font" required>
+                 <small class="form-text text-muted">รหัสนี้มีไว้สำหรับเปิดสมุดกระจกส่วนตัวของคุณ เพื่อป้องกันไม่ให้เพื่อนคนอื่นเข้ามาดูข้อความส่วนตัวในสมุดกระจกของคุณ</small>
+              </div>
+            <button type="submit" name="submit" value="submit" class="btn button font">เพิ่มสมุดกระจกเงา</button>
         </form>
       </div>
+
+          <!-- เซฟข้อมูลเข้า Database ยังไง? -->
 
   </body>
 </html>
